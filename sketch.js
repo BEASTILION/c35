54a63d77d6bbb014a8d536e5d83843479d99cfdb
BEASTILION <94374957+BEASTILION@users.noreply.github.com>
2022-01-17 19:47:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+var rball;
+var database,position;
+
+function setup(){
+    database = firebase.database();
+    createCanvas(500,500);
+    rball = createSprite(250,250,10,10);
+    rball.shapeColor = "red";
+
+    var rballposition = database.ref('ball/position');
+    rballposition.on("value",readPosition,showError)
+
+
+
+
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+    database.ref('ball/position').set({
+        'x':position.x+x,
+        'y':position.y+y 
+
+    })
+  
+}
+
+function readPosition(data){
+position = data.val();
+rball.x = position.x
+rball.y = position.y
+
+
+}
+
+function showError(){
+
+console.log("Data didnt recieved from Firebase")
+
+}
